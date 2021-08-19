0a238529e794bb2d7dc587bc77eda1137cead3c7
Amanda KIBENDE <contactproamanda@gmail.com>
2021-08-19 17:51:38 +0200

    install formMeme

---
+++ src/components/Forms/FormMeme.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class FormMeme extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FormMeme
+
