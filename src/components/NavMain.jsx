100ca4b143f26e65364d81fcc02f8d943dd1e4ca
abdou91140 <m.abdoucisse91140@gmail.com>
2021-08-24 11:43:57 +0200

    navMain in progress

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -20,35 +20,55 @@ const NavMain = (props) => {
   }
 
   return (
-    <nav className="NavMain">
-      <NavLink exact to="/">
-        <h3 className="logo">App name</h3>
-      </NavLink>
-      <ul className="nav-list">
-        {context.isLoggedIn && (
-          <React.Fragment>
-            <li>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <NavLink exact to="/" className="navbar-item">
+          <img className="image" src="../memeLogo.png" alt="" width="200" height="100" />
+        </NavLink>
+
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <NavLink exact to="/" className="navbar-item">
+            Home
+          </NavLink>
+        </div>
+
+        <div className="navbar-end">
+          {context.isLoggedIn && (
+            <React.Fragment>
+
               <NavLink to="/profile">
                 {context.user && context.user.email}
               </NavLink>
-            </li>
-            <li>
-              <p onClick={handleLogout}>Logout</p>
-            </li>
-          </React.Fragment>
-        )}
-        {!context.isLoggedIn && (
-          <React.Fragment>
-            <li>
-              <NavLink to="/signin">Log in</NavLink>
-            </li>
-            <li>
-              <NavLink to="/signup">Create account</NavLink>
-            </li>
-          </React.Fragment>
-        )}
-      </ul>
-    </nav>
+
+              <button className="button" onClick={handleLogout}>Logout</button>
+
+            </React.Fragment>
+          )}
+          {!context.isLoggedIn && (
+            <React.Fragment>
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-light"> <NavLink to="/signin">Log in</NavLink></a>
+
+                  <button className="button is-primary">
+                    <NavLink to="/signup">Create account</NavLink>
+                  </button>
+                </div>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </nav >
+
   );
 };
 
