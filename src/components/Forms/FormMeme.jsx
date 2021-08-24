292d57adb8c4156adde3e8d6a1db88ac1bba8888
Amanda KIBENDE <contactproamanda@gmail.com>
2021-08-24 09:44:31 +0200

    update mardi matin

--- src/components/Forms/FormMeme.jsx
+++ src/components/Forms/FormMeme.jsx
@@ -9,130 +9,123 @@ import SearchBar from "./SearchBar";
 <SearchBar />;
 
 class FormMeme extends Component {
-    state = {
-        caption1: "",
-        caption2: "",
-        memeimage: "",
-        name: "",
-        search: "",
+  state = {
+    caption1: "",
+    caption2: "",
+    image: "",
+    name: "",
+    search: "",
+  };
+
+  handleChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+    this.setState({ [key]: value });
+  };
+
+  handleSearchValue = (event) => {
+    console.log("target value !!!!!!", event);
+
+    this.setState({
+      search: event,
+    });
+  };
+
+  handleCreate = (event) => {
+    event.preventDefault();
+    const meme = {
+      caption1: this.state.caption1,
+      caption2: this.state.caption2,
+      imageApi: this.state.imageApi,
     };
 
-    handleChange = (event) => {
-        if (this.state.search !== "") {
-            event.target = this.state.search
-        }
-        const key = event.target.name;
-        const value = event.target.value;
-        this.setState({ [key]: value });
-    };
+    apiHandler
+      .postMemes(meme)
+      .then((data) => {
+        console.log("here", data);
+        this.setState(...data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    handleClickImage = (event) => {
-        event.preventDefault()
-        const value = event.target.src;
-        this.setState({ memeimage: value });
-    };
+  render() {
+    const filteredImage = Imagedb.filter((image) => { 
 
-    handleSearchValue = (event) => {
-        this.setState({
-            search: event,
-        });
-    };
 
-    handleFileUpload = (event) => {
-        this.setState({ memeimage: event.target.files[0] });
-    }
+      if(this.state.search === "") return false;
+       return image.name.toLowerCase().includes(this.state.search.toLocaleLowerCase())
+    });
 
-    handleCreate = (event) => {
-        event.preventDefault();
-        const meme = {
-            caption1: this.state.caption1,
-            caption2: this.state.caption2,
-            imageApi: this.state.imageApi,
-        };
+    return (
+      <form forHTML="meme" onSubmit={this.handleCreate}>
+        <h1>Meme's form !</h1>
 
-        apiHandler
-            .postMemes(meme)
-            .then((data) => {
-                console.log("here", data);
-                this.setState(...data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
+        <div>
+          <SearchBar
+            handleChange={this.handleSearchValue}
+            value={this.state.search}
+            onChange = {this.handleOnChange}
+          />
+        </div>
 
-    render() {
-        console.log(this.state)
-        const filteredImage = Imagedb.filter((image) => {
-            if (this.state.search === "") return false;
-            return image.name.toLowerCase().includes(this.state.search.toLocaleLowerCase())
-        });
+        <div className="box">
+          {filteredImage.map((image) => {
+            return (
+              <div className="box">
+                <article className="media">
+                  <div className="media-left">
+                    <figure className="image is-64x64">
+                      <img src={image.image} alt="" />
+                    </figure>
+                  </div>
+                </article>
+              </div>
+            );
+          })}
+        </div>
 
-        return (
-            <div className="container">
-                <form forHTML="meme" onSubmit={this.handleCreate}>
-                    <h1 className="title">Meme's form !</h1>
+        <div>
+          <label htmlFor="caption1">Text 1 : </label>
+          <input
+            type="text"
+            id="caption1"
+            name="caption1"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="caption2">Text 2 : </label>
+          <input
+            type="text"
+            id="caption2"
+            name="caption2"
+            onChange={this.handleChange}
+          />
+        </div>
 
-                    <div>
-                        <SearchBar
-                            handleChange={this.handleSearchValue}
-                            value={this.state.search}
-                            onChange={this.handleOnChange}
-                        />
-                    </div>
+        <div>
+          <label htmlFor="memeimage" action="/upload">
+            Meme: :{" "}
+          </label>
+          <input
+            type="file"
+            id="memeimage"
+            name="memeimage"
+            onChange={this.handleFileChange}
+          />
+        </div>
+        <div>
+          <input type="button" onClick={this.upload} value="Upload" />
+        </div>
 
-                    <div className="box">
-                        {filteredImage.map((image) => {
-                            return (
-                                <div className="box" key={image.name} onClick={this.handleClickImage}>
-                                    <img className="image" src={image.image} alt="" />
-                                </div>
-                            );
-                        })}
-                    </div>
-
-                    <div>
-                        <label className="label" htmlFor="caption1">Caption 1 : </label>
-                        <input
-                            className="input"
-                            type="text"
-                            id="caption1"
-                            name="caption1"
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label className="label" htmlFor="caption2">Caption 2 : </label>
-                        <input
-                            className="input"
-                            type="text"
-                            id="caption2"
-                            name="caption2"
-                            onChange={this.handleChange}
-                        />
-                    </div>
-
-                    <div>
-                        <label className="label" htmlFor="memeimage" action="/upload">
-                            Meme: :{" "}
-                        </label>
-                        <input
-                            className="input"
-                            type="file"
-                            id="memeimage"
-                            name="memeimage"
-                            onChange={this.handleFileUpload}
-                        />
-                    </div>
-                    {this.state.memeimage && <div><input type="text" name="caption1" className="input" placeholder={this.state.caption1} /><img className="image" src={this.state.memeimage} alt="" /></div>}
-
-                    <div>
-                        <button className="button " type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default FormMeme;
