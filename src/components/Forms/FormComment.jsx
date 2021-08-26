261188261db1676ff9111125a78b7781e70bdca4
MinaTough <mina.toughrani@gmail.com>
2021-08-26 12:24:50 +0200

    updates delete comment

--- src/components/Forms/FormComment.jsx
+++ src/components/Forms/FormComment.jsx
@@ -1,46 +1,47 @@
 import React, { Component } from "react";
 import apiHandler from "../../api/apiHandler";
 
-
 class FormComment extends Component {
-    state = {
-        comment: "",
-    };
+  state = {
+    comment: "",
+  };
 
-    handleChange = (event) => {
-        this.setState({ comment: event.target.value });
-    };
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("successfully posted!");
+    apiHandler
+      .postComment(this.props.memeById, this.state)
+      .then(() => {
+        this.props.updateComments();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        apiHandler
-            .postComment(this.props.memeById, this.state)
-            .then(() => {
-
-                this.props.updateComments()
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    render() {
-        return (
-            <form className="has-addons" onSubmit={this.handleSubmit}>
-                <div className="control">
-                    <span>  <input
-                        className="input is-small"
-                        type="text"
-                        name="comment"
-                        onChange={this.handleChange}
-                        required
-                    />
-                        <button className="button is-small">Add</button></span>
-                </div>
-            </form >
-        );
-    }
+  render() {
+    return (
+      <form className="has-addons" onSubmit={this.handleSubmit}>
+        <div className="control">
+          <span>
+            {" "}
+            <input
+              className="input is-small"
+              type="text"
+              name="comment"
+              onChange={this.handleChange}
+              required
+            />
+            <button className="button is-small">Add</button>
+          </span>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default FormComment;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,7 @@ import apiHandler from "../api/apiHandler";
 import "bulma/css/bulma.css";
 import Signin from "./Signin";
 import FormComment from "../components/Forms/FormComment";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Home extends Component {
   state = {
@@ -14,9 +12,8 @@ class Home extends Component {
     memeId: null,
     comments: [],
     closeComment: false,
-    addComment: false
-  }
-
+    addComment: false,
+  };
 
   componentDidMount() {
     apiHandler
@@ -26,41 +23,52 @@ class Home extends Component {
           memes: dbRes,
         });
       })
-      .catch((e) => console.log(e))
-
-
+      .catch((e) => console.log(e));
   }
+
+  handleDelete = (commentId, memeId) => {
+
+    apiHandler
+      .deleteComments(memeId, commentId)
+      .then(() => {
+  
+
+     
+      })
+      .catch((e) => console.log(e));
+  };
 
   handleClick = (id) => {
     if (id === this.state.memeId) {
-      this.setState({ memeId: null, comments: [] })
+      this.setState({ memeId: null, comments: [] });
     } else {
-      this.setState({ memeId: id })
+      this.setState({ memeId: id });
       apiHandler
         .getComments(id)
         .then((dbRes) => {
-          console.log(dbRes)
+          console.log(dbRes);
+
           this.setState({
-            comments: dbRes
-          })
+            comments: dbRes,
+          });
         })
-        .catch((e) => console.log(e))
+        .catch((e) => console.log(e));
     }
-  }
+  };
   handleAddComment = (id) => {
-    this.setState({ memeId: id, closeComment: false })
+    this.setState({ memeId: id, closeComment: false });
     apiHandler
       .getComments(id)
       .then((dbRes) => {
         this.setState({
-          comments: dbRes
-        })
+          comments: dbRes,
+        });
       })
-      .catch((e) => console.log(e))
-  }
+      .catch((e) => console.log(e));
+  };
   addComment = () => {
-    this.setState({ addComment: !this.state.addComment })
-  }
+    this.setState({ addComment: !this.state.addComment });
+  };
   render() {
     return (
       <div>
@@ -81,23 +89,39 @@ class Home extends Component {
                   <div className="imgBox">
                     <img src={meme.memeimage} alt="" className="img" />
 
-                    <div className="button is-small" onClick={() => {
-                      this.handleClick(meme._id)
-                    }} ><FontAwesomeIcon icon="chevron-down" /></div><span className="button is-small" onClick={this.addComment}>Comment</span>{this.state.addComment && <FormComment updateComments={this.handleAddComment} memeById={meme._id} />}
-                    {this.state.memeId === meme._id && this.state.comments.map((comment) => { return <div className="box" key={comment._id}><span>{comment.text}</span></div> })}
                     <span className="topText">{meme.caption1}</span>
                     <span className="bottomText">{meme.caption2}</span>
                   </div>
-                  <span>
-                    <img
-                      className="icon"
-                      src="../comment-icon.png"
-                      alt="icon-comment"
+                  <div
+                    className="button is-small"
+                    onClick={() => {
+                      this.handleClick(meme._id);
+                    }}
+                  >
+                    <FontAwesomeIcon icon="chevron-down" />
+                  </div>
+                  <span className="button is-small" onClick={this.addComment}>
+                    Comment
+                  </span>
+                  {this.state.addComment && (
+                    <FormComment
+                      updateComments={this.handleAddComment}
+                      memeById={meme._id}
                     />
-                  </span>
-                  <span>
-                    <Likebutton />
-                  </span>
+                  )}
+                  {this.state.memeId === meme._id &&
+                    this.state.comments.map((comment) => {
+                      return (
+                        <div className="box" key={comment._id}>
+                          <span>
+                            {comment.text} posted by: {comment.creator.userName}
+                          </span>
+                          <button onClick={() => this.handleDelete(comment._id, meme._id)}>Delete</button>
+                        </div>
+                      );
+                    })}
+                  <span></span>
+                  <span></span>
                 </article>
               </div>
             );
