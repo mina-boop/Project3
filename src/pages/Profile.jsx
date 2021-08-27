0b354f3912951fb2c4d46bd325dc3b3736a541e2
abdou91140 <m.abdoucisse@gmail.com>
2021-08-27 14:20:20 +0200

    profile css

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { withUser } from "../components/Auth/withUser";
 import apiHandler from "../api/apiHandler";
 import "bulma/css/bulma.css";
 import "../styles/profile.css";
+import "../styles/Home.css";
 
 class Profile extends Component {
   state = {
@@ -44,56 +45,45 @@ class Profile extends Component {
 
   render() {
     return (
-      <div className="profile-container">
-        <h2>Welcome {this.state.profile.userName}</h2>
-
-        <section className="profile">
-        <figure className="image is-128x128">
-            
-              <img className="is-rounded image-profile" 
-                src={this.state.profile.profileImg}
-                alt={this.state.profile.userName}
-              />
-           
-          </figure>
-          <div className="user-presentation">
-            
-            <Link className="link" to="/profile/settings">
-              <h3>Edit profile</h3>
-            </Link>
+      <div>
+        <div className="container">
+          <div className="card">
+            <div className="card-content">
+              <div className="media-content">
+                <div className="media-left">
+                  <img className="profile-picture"
+                    src={this.state.profile.profileImg}
+                    alt={this.state.profile.userName}
+                  />
+                </div>
+              </div>
+              <div className="media-content">
+                <p className="title is-3">{this.state.profile.userName}</p>
+                <p className="subtitle is-6">{this.state.profile.city}</p>
+                <p className="title is-6">{this.state.profile.email}</p>
+                <p className="subtitle is-6">{this.state.profile.zodiacSign}</p>
+              </div>
+            </div>
           </div>
-
-          <div className="memes">
-            <h3>My memes</h3>
-            
-             
-                {this.state.meme.map((meme) => {
-                  return (
-                    
-                      <article className="box" key={meme._id}>
-                        <p>Posted </p>
-                        <img src={meme.memeimage} alt="" />
-                        <div className="buttons">
-                          <span>
-                            <button
-                              id={meme._id}
-                              className="btn-secondary"
-                              onClick={this.handleDelete}
-                            >
-                              Delete
-                            </button>
-                          </span>
-                        </div>
-                      </article>
-                   
-                  );
-                })}
-             
-            
-          </div>
-        </section>
+        </div>
+        <div className="container">
+          {this.state.meme.map((meme) => {
+            return (
+              <div className="card is-small">
+                <div className="card-content" >
+                  <div className="imgBox">
+                    <img src={meme.memeimage} alt="" className="img" />
+                    <span className="topText">{meme.caption1}</span>
+                    <span className="bottomText">{meme.caption2}</span>
+                  </div>
+                  <button id={meme._id} className="button is-primary" onClick={this.handleDelete}>Delete</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
-    );
+    )
   }
 }
 
